7835c7ff83c2d261a2f37356610ea5e6f0722d8c
itrobotom <121325608+itrobotom@users.noreply.github.com>
2023-03-16 05:59:08 +0700

    Переписал немного основной файл.
    Перенес некоторые некоторые функции  в основной.
    Надо еще проработать мутации (splice), и завернуть в отдельные функции. В render лапша получилась.

--- js/main.js
+++ js/main.js
@@ -1,85 +1,137 @@
 import * as massModule from './mainMass.js';
 
-let countId = 1;
+let countId = 1; //счетчик для уникального id каждой задаче
 
-let taskHighList;
+//кнопка добавления задачи для любого приоритета (две кнопки на странице)
+const btnAdd = document.querySelectorAll(".btn");
+
 let htmlTemplateTask = '<li><div class="input-task"><div class="radio-btn"><input class="task-checkbox-input" type="checkbox"></div><div class="input-task-text">Поле по умолчанию</div><button class="btn"><img class = "icon-del-task" id="del-task-high" src="./img/close-icon.svg" alt="close task"></button></div></li>'
 
-//по заданию нужно на enter отправить форму, но нужно отменить стандартное действие, иначе страница перезагрузиться и форма исчезнет
-//получим по id форму
-const formTaskHigh = document.getElementById("add-task-form-input");
-//для добавления действия по enter сбросим стандартное поведение при type="submit" element.prevetnDefault
-formTaskHigh.addEventListener('submit', function(e) {
-  // сбрасываем стандартное поведение функции function(e) (по умолчанию она делает get запрос) и после нажатия на enter форма не сбросится и не перезагрузится
-  e.preventDefault();
+//слушаем нажатие одной из двух (пока из двух) кнопок в форме ввода задачи и вызываем функцию добавления задачи в случае нажатия кнопки
+btnAdd.forEach(btnAdd => {
+	btnAdd.addEventListener("click", addTask);
+});
 
-  //добавляем задачу по нажатю на enter из поля
-  if (formTaskHigh.highTask.value === ""){
-    alert("Поле для добавления задачи пустое");
-  } else {
-    massModule.addTask(massModule.todoArr, formTaskHigh.highTask.value, massModule.PRIORIRTIES.HIGH, countId);   
-  }
-  //massModule.showTodo(massModule.todoArr);
-  console.log(massModule.todoArr);
+function addTask(e) {
+  e.preventDefault(); //сброс действия по умолчанию для отправки формы чтобы страница не перезагружалась
  
+  //проверяем, в каком поле появилось значение (в LOW или HIGH)
+  const inputHigh = document.querySelector(".input-high").value;
+  const inputLow = document.querySelector(".input-low").value;
+  
+  if (inputHigh){
+    console.log('Добавляем новую задачу HIGH в массив!');
+    //ОПТИМИЗИРОВАТЬ И ПРОПИСАТЬ ВЫЗОВ ФУНКЦИИ ПО ДОБАВЛЕНИЮ ЗАДАЧИ НУЖНОГО СТАТУСА
+    if(massModule.todoArr.find(element => element.name === inputHigh)){
+      alert('Такая задача уже есть!');
+    } else {
+      const newTaskObj = {name: inputHigh, status: massModule.STATUSES.IN_PROGRESS, priority: massModule.PRIORIRTIES.HIGH, id: countId};
+      massModule.todoArr.push(newTaskObj);
+      console.log(massModule.todoArr);
+    }
+
+  } else if (inputLow){
+    console.log('Добавляем новую задачу LOW в массив!');
+    const newTaskObj = {name: inputLow, status: massModule.STATUSES.IN_PROGRESS, priority: massModule.PRIORIRTIES.LOW, id: countId};
+    massModule.todoArr.push(newTaskObj);
+    console.log(massModule.todoArr);
+    //const listLow = document.getElementById("task-low-list");
+    //listLow.insertAdjacentHTML("beforeend", htmlTemplateTask);
+  } else {
+    alert('Вы не ввели задачу');
+  }
+  //чистим поле после ввода задачи
+  document.querySelector(".input-high").value = "";
+  document.querySelector(".input-low").value = "";
+  
   render(massModule.todoArr); 
 
   countId += 1; //для добавления уникального id задачам
-});
+}
 
 //функция для обновления задач на странице 
+//ДОБАВИТЬ АРГУМЕНТ СТАТУСА, ДЛЯ КАКОГО СТАТУСА МЫ ВЫЗЫВАЕМ ЭТУ ФУНКЦИЮ
+//ИЛИ ГДЕ ЛУЧШЕ УЧИТЫВАТЬ ДОБАВЛЕНИ
 function render(arrTask) {
-  let elementsDel = document.getElementById('task-high-list');
-
-  while (elementsDel.firstChild) {
-    elementsDel.removeChild(elementsDel.firstChild);
+  
+  //ОБЕРНУТЬ В ФУНКЦИЮ ОТЧИСТКУ DOM ДЕРЕВА
+  //сначала удаляет все задачи из списка в DOM дереве, для начала найдем списки
+  const listHigh = document.getElementById('task-high-list');
+  const listLow = document.getElementById('task-low-list');
+  //console.log(`Первый дочерний элемент: ${elementsDel.firstChild}`);
+  //пока есть хоть один (первый) дочерный элемент удалаем его в обоих списках
+  while (listHigh.firstChild) {
+    listHigh.removeChild(listHigh.firstChild);
   }
-
-  let arrPrior = massModule.getTodoPriorityId(arrTask, massModule.PRIORIRTIES.HIGH);
-  arrPrior.forEach(function(elem) {
-    taskHighList = document.getElementById("task-high-list");
-    taskHighList.insertAdjacentHTML("beforeend", htmlTemplateTask);
-
-    const parent = document.querySelector('#task-high-list'); //поиск по id через решетку - #task-high-list
-    const newTemplate = parent.lastElementChild;
-    console.log(newTemplate.querySelector(".input-task-text").innerHTML); //поиск по классу через точку - .input-task-text
-    newTemplate.querySelector(".input-task-text").innerHTML = elem.name; //записываем задачу
-    newTemplate.setAttribute("id", elem.id);
-    console.log(`Задача ${elem.name} с HIGH приоритетом `);
-    let getElemForChangeColor = newTemplate.querySelector(".input-task-text");
-    if(elem.status === massModule.STATUSES.IN_PROGRESS){
-      getElemForChangeColor.style.background = 'green';
-    } else { //иначе красный
-      getElemForChangeColor.style.background = 'red';
+  while (listLow.firstChild) {
+    listLow.removeChild(listLow.firstChild);
+  }
+  
+  //ОБЕРНУТЬ В ФУНКЦИЮ ДОБАВЛЕНИЕ ЗАДАЧИ ИЗ МАССИВА В DOM ДЕРЕВО с проверкой добавляемого статуса и отрисовкой цвета
+  //далее берем актуальные задачи из массива и в зависимости от приоритета добавляем в нужные позиции
+  massModule.todoArr.forEach(function(elem){
+    if(elem.priority === massModule.PRIORIRTIES.HIGH){
+      listHigh.insertAdjacentHTML("beforeend", htmlTemplateTask); //добавили шаблон
+      //добавим в шаблон введенную задачу
+      //последняя конструкция дочерняя отностиельно списка с HIGH зачадачами
+      const newTemplate = listHigh.lastElementChild;
+      newTemplate.querySelector(".input-task-text").innerHTML = elem.name; //записываем задачу
+      //добавим id из массива с задачами нашей таске в DOM
+      newTemplate.setAttribute("id", elem.id);
+      //установка цвета по статусу
+      const getElemForChangeColor = newTemplate.querySelector(".input-task-text");
+      //****ПРОВЕРИТЬ СТАТУС ТАСКИ ИЗ МАССИВА
+      
+      //если статус DONE, поставить цвет элементу зеленый
+      if(elem.status === massModule.STATUSES.IN_PROGRESS){
+        getElemForChangeColor.style.background = 'green';
+      } else { //иначе красный
+        getElemForChangeColor.style.background = 'blue';
+      }
+    } 
+    else if(elem.priority === massModule.PRIORIRTIES.LOW){
+      listLow.insertAdjacentHTML("beforeend", htmlTemplateTask); //добавили шаблон
+      const newTemplate = listLow.lastElementChild;
+      newTemplate.querySelector(".input-task-text").innerHTML = elem.name; //записываем задачу
+      //добавим id из массива с задачами нашей таске в DOM
+      newTemplate.setAttribute("id", elem.id);
+      //установка цвета по статусу
+      const getElemForChangeColor = newTemplate.querySelector(".input-task-text");
+      //****ПРОВЕРИТЬ СТАТУС ТАСКИ ИЗ МАССИВА
+      //если статус DONE, поставить цвет элементу зеленый
+      if(elem.status === massModule.STATUSES.IN_PROGRESS){
+        getElemForChangeColor.style.background = 'green';
+      } else { //иначе красный
+        getElemForChangeColor.style.background = 'red';
+      }
     }
-    let idTask = elem.id;
-    let elemChange = document.getElementById(idTask);
-    let buttonDel = elemChange.querySelector("#del-task-high");
+
+    //слушаем кнопку удаления только тогда, когда отрисовали задачу!!!
+    //найдем сначала задачу по id а затем и кнопку для удаления задачи
+    const idTask = elem.id;
+    const elemChange = document.getElementById(idTask); //находим id конкретного элемента
+    const buttonDel = elemChange.querySelector(".btn"); //в нем находим кнопку
     //слушаем нажатие кнопки для удаления задачи
     buttonDel.addEventListener("click", function(){
-      //получим id элемента, где нажали крестик для удаления
-      console.log(`id элемента для удаления: ${newTemplate.id}`);
       deleteTaskFromArr(massModule.todoArr, idTask);
     });
-
+      
+    //слушаем чекбокс задачи (задачу elemChange по id мы нашли выше)
     let stat = elem.status; //считываем текущий статус задачи
     let checkbox = elemChange.querySelector(".task-checkbox-input");
     checkbox.addEventListener("click", function() {
       changeStatus(massModule.todoArr, idTask, stat)
-    });
+    }); 
   });
-  console.log(`Массив элементов ${arrPrior} с приоритетом HIGH`);
 }
 
 //функция удаления ЗАДАЧИ из массива по id
 function deleteTaskFromArr(arr, idTask){
-    //проверять, есть ли такая задача не надо, т.к. уже нашли ее id, а если он есть, значит есть и задача для удаления
-    let delTaskConsole = arr.splice(arr.findIndex(element => element.id == idTask), 1);
-    console.log(`Задача "${delTaskConsole[0].name}" успешно удалена`);
-    render(massModule.todoArr);  
+  //проверять, есть ли такая задача не надо, т.к. уже нашли ее id, а если он есть, значит есть и задача для удаления
+  let delTaskConsole = arr.splice(arr.findIndex(element => element.id == idTask), 1);
+  render(massModule.todoArr);  
 }
 
-//функция смены статуса задачи по id
 function changeStatus(arr, idTask, stat) {
   arr.findIndex(function(element) {
     if(element.id === idTask) {
@@ -92,3 +144,25 @@ function changeStatus(arr, idTask, stat) {
   });
   render(massModule.todoArr);  
 }
+
+/*
+//функция смены статуса задачи по id с учетом нового статуса newStat
+//при однократном нажатии сменяется статус на todo 
+//при двукратном нажатии на inProgress
+//при трехкратном снова устанавливается статус Done
+function changeStatus2(arr, idTask, newStat) {
+  arr.findIndex(function(element) {
+    if(element.id === idTask) {
+      for (let key in element){
+
+      }
+      if(stat === massModule.STATUSES.IN_PROGRESS){
+        element.status = massModule.STATUSES.DONE;
+      } else {
+        element.status = massModule.STATUSES.IN_PROGRESS;
+      }
+    }
+  });
+  render(massModule.todoArr);  
+}
+*/
--- js/mainMass.js
+++ js/mainMass.js
@@ -1,5 +1,5 @@
 export const STATUSES = { //пространство имен задаем, когда есть изначально известные часто используемые в коде данные (строки, числа)
-    //TO_DO: 'To Do',
+    TO_DO: 'To Do',
     IN_PROGRESS: 'In progress',
     DONE: 'Done',
 }; 
@@ -10,27 +10,9 @@ export const PRIORIRTIES = {
     HIGH: 'High',
 }
   
-export const statusDefault = 'To Do';
-  
 export let todoArr = [ 
     {name: 'create a new practice task', status: STATUSES.IN_PROGRESS, priority: PRIORIRTIES.HIGH, id: 0}, 
+    /*{name: 'create a new practice task №2', status: STATUSES.IN_PROGRESS, priority: PRIORIRTIES.HIGH},*/ 
     {name: 'make a bed', status: STATUSES.DONE, priority: PRIORIRTIES.HIGH}, 
+    /*{name: 'make a bed2', status: STATUSES.DONE, priority: PRIORIRTIES.LOW},*/
 ];
-
-export function addTask(arr, newTask, prior, idNow){
-    //добавить задачу, если такой еще нет
-    //если есть, сообщить что такая уже есть
-    if (arr.find(element => element.name === newTask)){ 
-        console.log('Такая зачача уже есть!');
-    } else { //иначе добавить задачу
-        let newTaskObj = {name: newTask, status: STATUSES.IN_PROGRESS, priority: prior, id: idNow};
-        arr.push(newTaskObj);
-    }
-}
-
-//возвращает МАССИВ ОБЪЕКТОВ ЗАДАЧ С НУЖНЫМ ПРИОРИТЕОТОМ, УНИКЛЬНЫМ ID, статусом и остальными данными, которые не будем использовать
-export function getTodoPriorityId(arr, priority) {
-    const arrPriorObj = arr.filter(element => element.priority === priority);
-    console.log(arrPriorObj); 
-    return arrPriorObj;
-}
